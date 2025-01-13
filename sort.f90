25fca37b5d97cd1665035b41c7c0387a46958fa6
austinl1905 <austinl@DESKTOP-RCELDJA>
2025-01-12 22:51:50 -0500

    add merge sort implementation

---
+++ sort.f90
@@ -0,0 +1,63 @@
+MODULE MERGE_SORT
+    IMPLICIT NONE
+CONTAINS
+    RECURSIVE FUNCTION SORT(ARR) RESULT(RES)
+        INTEGER, DIMENSION(:), ALLOCATABLE, INTENT(IN) :: ARR
+        INTEGER, DIMENSION(:), ALLOCATABLE :: LEFT, RIGHT, RES
+        INTEGER :: MIDPOINT
+
+        IF (SIZE(ARR).LE.1) THEN ! BASE CASE
+            RES = ARR
+            RETURN
+        END IF
+        
+        MIDPOINT = SIZE(ARR) / 2
+        ! ASSIGN SUBARRAYS
+        LEFT = ARR(1:MIDPOINT)
+        RIGHT = ARR(MIDPOINT + 1:SIZE(ARR))
+
+        ! GO BACK UP CALL STACK ONCE SIZE OF SUBARRAYS REACHES 1
+        LEFT = SORT(LEFT)
+        RIGHT = SORT(RIGHT)
+
+        ! MERGE ARRAYS IN ASCENDING ORDER AND GO UP CALL STACK AGAIN
+        RES = MERGE_ARRAYS(LEFT, RIGHT)
+        RETURN
+    END FUNCTION
+
+    FUNCTION MERGE_ARRAYS(LEFT, RIGHT) RESULT(RES)
+        INTEGER, DIMENSION(:), ALLOCATABLE :: LEFT, RIGHT, RES
+        INTEGER :: LI, RI, I ! ITERATION VARIABLES FOR SUBARRAYS AND OUTPUT
+
+        LI = 1
+        RI = 1
+        I = 1
+        ALLOCATE(RES(SIZE(LEFT) + SIZE(RIGHT)))
+
+        DO WHILE (LI.LE.SIZE(LEFT).AND.RI.LE.SIZE(RIGHT)) ! ADD SMALLER ELEMENTS TO OUTPUT
+            IF (LEFT(LI).LT.RIGHT(RI)) THEN
+                RES(I) = LEFT(LI)
+                I = I + 1
+                LI = LI +  1
+            ELSE
+                RES(I) = RIGHT(RI)
+                I = I + 1
+                RI = RI + 1
+            END IF
+        END DO
+
+        ! ADD REMAINING ELEMENTS TO OUTPUT
+        DO WHILE (LI.LE.SIZE(LEFT))
+            RES(I) = LEFT(LI)
+            I = I + 1
+            LI = LI + 1
+        END DO
+
+        DO WHILE (RI.LE.SIZE(RIGHT))
+            RES(I) = RIGHT(RI)
+            I = I + 1
+            RI = RI + 1
+        END DO
+        RETURN
+    END FUNCTION
+END MODULE MERGE_SORT
